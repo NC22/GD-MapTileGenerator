78683edd7b482131c0e288b1a8c031b26340bcaf
Vladimir <torrenttvi@gmail.com>
2017-12-24 23:29:49 +0300

    Update example.php
    show tile map on whole page in example

--- example.php
+++ example.php
@@ -9,13 +9,28 @@ class Example {
     private $root; 
     private $mapFile = 'test';
     private $mapDir = 'tiles/'; // back slash at the end is important
-    private $mapZoomMax;
+    
+    // information about current or old uploaded map
+    private $mapZoomMax = false;
+    private $mapExist = false;
 
     private $log = '';
 
     public function __construct() {
         $this->root = dirname(__FILE__) . '/';
         require_once($this->root . 'GDMapTileGenerator.class.php');
+                    
+        // restore tiles info - I usually save info about map zoom in database
+        
+        $mapDirAbs = $this->root . $this->mapDir;
+        if (is_dir($mapDirAbs)) {
+            $zoomMax = 0;
+            while(is_dir($mapDirAbs . $zoomMax)) $zoomMax++;
+            if ($zoomMax) $zoomMax--;
+            
+            $this->mapZoomMax = $zoomMax;
+            $this->mapExist = true;
+        }
     }
 
     public function log($err) {
@@ -27,49 +42,56 @@ class Example {
         $this->mode = empty($_GET['mode']) ? 'default' : $_GET['mode'];
         if ($this->mode == 'upload') {
             
-            if (empty($_POST['image-upload'])) $this->show('Error, form not sended');
-            
-            if (empty($_FILES['image-file']['tmp_name']) or !is_uploaded_file($_FILES['image-file']['tmp_name'])) $this->show('Upload file fail');
-            
-            $extension = strtolower(substr($_FILES['image-file']['name'], 1 + strrpos($_FILES['image-file']['name'], ".")));
-            $file = $this->root . $this->mapFile . '.' . $extension;
-            
-            if (file_exists($file)) unlink($file);
-            
-            if (!move_uploaded_file($_FILES['image-file']['tmp_name'], $file)) $this->show('Move uploaded file fail');
-            
-            chmod($file, 0664);
-            
-            $generator = new Kelly\GDMapTileGenerator($file, false, true);
-            $generator->set('callback', array($this, 'log')); //  '\Kelly\Tool::log'
-            $generator->set('ext', 'png'); 
-            $generator->set('storage', $this->mapDir);
-            
-            ini_set("memory_limit", "-1");
-            ini_set("max_execution_time", 0);
-            
-            if ($generator->exec()) {
-            
-                $this->mapZoomMax = $generator->get('maxZoom');
-                if (!$this->mapZoomMax) $this->mapZoomMax = 2;
+            if (!empty($_POST['image-upload'])) {
                 
-            } else $this->show('Tile map generation fail');
+                if (empty($_FILES['image-file']['tmp_name']) or !is_uploaded_file($_FILES['image-file']['tmp_name'])) $this->show('Upload file fail');
+                
+                $extension = strtolower(substr($_FILES['image-file']['name'], 1 + strrpos($_FILES['image-file']['name'], ".")));
+                $file = $this->root . $this->mapFile . '.' . $extension;
+                
+                if (file_exists($file)) unlink($file);
+                
+                if (!move_uploaded_file($_FILES['image-file']['tmp_name'], $file)) $this->show('Move uploaded file fail');
+                
+                chmod($file, 0664);
+                
+                $generator = new Kelly\GDMapTileGenerator($file, false, true);
+                $generator->set('callback', array($this, 'log')); //  '\Kelly\Tool::log'
+                $generator->set('ext', 'png'); 
+                $generator->set('storage', $this->mapDir);
+                
+                ini_set("memory_limit", "-1");
+                ini_set("max_execution_time", 0);
+                
+                if ($generator->exec()) {
+                
+                    $this->mapZoomMax = $generator->get('maxZoom');
+                    if (!$this->mapZoomMax) $this->mapZoomMax = 2;
+                    
+                    $this->mapExist = true;
+                    
+                } else $this->show('Tile map generation fail');
+            }
+        
         } 
 
-        $this->show();
-        
+        $this->show();        
     }
 
 
     public function show($err = 'No errors') {
-
+        
+        $showMap = false;
+        if ($this->mode == 'show' and $err == 'No errors' and $this->mapZoomMax !== false) { 
+            $showMap = true;
+        }
     ?>
     <!DOCTYPE html>
     <html lang="en">
         <head>        
             <title>GDMapTileGenerator example</title>
             
-            <?php if ($this->mode == 'upload') { ?>
+            <?php if ($showMap) { ?>
                 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
                 <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
             <?php } ?>
@@ -80,15 +102,20 @@ class Example {
                 body, html {
                     padding : 0px;
                     margin : 0px;
+                    width : 100%;
+                    height : 100%;
+                }
+                
+                form {
+                    padding : 24px;
                 }
                 
                 body {
-                    padding : 24px;
                     background : #e2e2e2;
                 }
                 
                 #map-container {
-                    margin-top : 24px;
+                    margin-top : 0px;
                 }
                 
                 .notice {
@@ -104,24 +131,34 @@ class Example {
         </head>
         
         <body>
-        
-            <p class="notice">
-                This file created for testing purposes only. It shows an example of creating map tile from image with help of GDMapTileGenerator class    
-            </p>
+                    
+            <?php if (!$showMap) { ?>
             
-            <form action="example.php?mode=upload" method="post" enctype='multipart/form-data'>
-                <p>Choose an image (Upload max size : <?php echo ini_get('upload_max_filesize'); ?> | Post data max size : <?php echo ini_get('post_max_size'); ?>)</p>
+                <form action="example.php?mode=upload" method="post" enctype='multipart/form-data'>
+                    <?php if ($this->mapExist) { ?>
+                        <p> You uploaded some tilemap. <a href="?mode=show">SHOW</a>
+                    <?php } ?>
+                    <p class="notice">
+                        This file created for testing purposes only. It shows an example of creating map tile from image with help of GDMapTileGenerator class    
+                    </p>
+                    <p>Choose an image (Upload max size : <?php echo ini_get('upload_max_filesize'); ?> | Post data max size : <?php echo ini_get('post_max_size'); ?>)</p>
+                    
+                    <input type="hidden" name="image-upload" value="1">
+                    <input type="file" name="image-file">
+                    <input type="submit" value="Upload">
+                                   
+                            
+                    <?php if ($err or $this->log) { ?>
+                            
+                        <p>Upload errors : <?php echo $err; ?></p>
+                        <p>Tile generator log : <br><?php echo $this->log; ?></p>
+                    
+                    <?php } ?>
+                </form>
                 
-                <input type="hidden" name="image-upload" value="1">
-                <input type="file" name="image-file">
-                <input type="submit" value="Upload">
-            </form>
+            <?php } elseif ($showMap) { ?>
             
-            <?php if ($this->mode == 'default') { ?>
-            
-            <?php } elseif ($this->mode == 'upload' and $err == 'No errors') { ?>
-            
-                <div id="map-container" style="width : 640px; height : 480px;"></div>    
+                <div id="map-container" style="width : 100%; height : 100%;"></div>    
                 
                 <script>
                     var maxZoom = <?php echo $this->mapZoomMax; ?>;
@@ -148,15 +185,7 @@ class Example {
                     mapLayer.addTo(map);
                 </script>
                 
-         <?php } ?>
-         
-               
-        <?php if ($err or $this->log) { ?>
-                
-            <p>Upload errors : <?php echo $err; ?></p>
-            <p>Tile generator log : <br><?php echo $this->log; ?></p>
-        
-        <?php } ?>
+         <?php } ?>              
         
         </body>
         
